2205012fcd00e4a278eef01fdadee5a45a342b49
Sami Abdul-Wahid <syntapy@pm.me>
2022-04-28 04:26:24 -0700

    remove unused is_ppo function

--- tomlkit/check.py
+++ tomlkit/check.py
@@ -1,13 +1,3 @@
-def is_ppo(v):
-    from datetime import date
-    from datetime import datetime
-    from datetime import time
-    from datetime import timedelta
-    PPO_TYPES = [int, bool, float, date, time, datetime, list, tuple, dict, str]
-    if type(v) in PPO_TYPES:
-        return True
-    return False
-
 def is_tomlkit(v):
     from .items import Item as _Item
     from .container import Container
